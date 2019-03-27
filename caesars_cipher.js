ea945ed7ee070c717171871c51838e97f9c0627e
Erdi Uçar <ucarerdi@gmail.com>
2019-03-27 20:18:00 +0300

    Update caesars_cipher.js

--- caesars_cipher.js
+++ caesars_cipher.js
@@ -7,16 +7,17 @@ function isAlpha(str) {
   return false;
 }
 
-// Ciphering string
+// Caesars cipher function
 function rot13(str) {
   // Convert string to upper case
   let notCipheredStr = str.toUpperCase();
   let cipheredStr = '';
 
-  // Checking all characters. If the char is alphabetic, sum with 13 on ASCII
+  // Check all characters
   for (let index = 0; index < notCipheredStr.length; index++) {
     const element = notCipheredStr[index];
-
+    
+    // If the char is alphabetic, sum with 13 on ASCII table
     if (isAlpha(element)) {
       cipheredStr += String.fromCharCode(((element.charCodeAt(0) - 65 + 13) % 26) + 65);
     } else {
